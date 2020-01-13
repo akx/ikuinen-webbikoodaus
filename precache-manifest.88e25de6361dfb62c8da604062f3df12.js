a549daafb7500d7462c1a459564e5f15582f5959
Aarni Koskela <akx@iki.fi>
2020-01-13 12:31:13 -0200

    Updates

---
+++ precache-manifest.88e25de6361dfb62c8da604062f3df12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96640a2635b28e0cba7f39aec21c20b4",
+    "url": "/ikuinen-webbikoodaus/index.html"
+  },
+  {
+    "revision": "1cd8724d9a44e6c134b6",
+    "url": "/ikuinen-webbikoodaus/static/css/main.e736972b.chunk.css"
+  },
+  {
+    "revision": "1a50f8a6349b134d9f5e",
+    "url": "/ikuinen-webbikoodaus/static/js/2.d21a2432.chunk.js"
+  },
+  {
+    "revision": "ccbc1d8768028cb10e33433b5c30a082",
+    "url": "/ikuinen-webbikoodaus/static/js/2.d21a2432.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cd8724d9a44e6c134b6",
+    "url": "/ikuinen-webbikoodaus/static/js/main.057d41a5.chunk.js"
+  },
+  {
+    "revision": "a4167801b2e5d3c868c0",
+    "url": "/ikuinen-webbikoodaus/static/js/runtime-main.f7c99739.js"
+  }
+]);
